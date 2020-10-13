6c954e200c7d43927c73236fb7836de6e8deb15b
870shintaro <71922097+870shintaro@users.noreply.github.com>
2020-10-13 13:34:12 +0900

    課題提出　演算子
    Task1-3.java作成

--- JavaStudy/Task1-3.java
+++ JavaStudy/Task1-3.java
@@ -22,7 +22,7 @@ public class Task1_3 {
         int multiply = 10 * 2;
 
         // ④ 20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
-        int remi = 20 - (20/6)*6;
+        int remi = 20 % 6;
         // ⑤下記の処理について、何をしているのかコメントを記入してください
         // [有楽町が入った文字列townと線が入ったlineを作成]
         String town = "有楽町";
